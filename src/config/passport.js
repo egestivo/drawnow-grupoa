ffe1f235ab375d15fdfbdca8e119b60525e4956f
ronaldpg <rjpuruncajas@espe.edu.ec>
2026-06-23 22:23:27 -0500

    Actualizando

--- src/config/passport.js
+++ src/config/passport.js
@@ -1,6 +1,6 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const Usuario = require('../models/Usuario');
+const Usuario = require('../server/models/Usuario');
 passport.use(new GoogleStrategy({
 clientID: process.env.GOOGLE_CLIENT_ID,
 clientSecret: process.env.GOOGLE_CLIENT_SECRET,
